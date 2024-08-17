7647e68a25a6579a27de94a393b317587e524eb3
paulobr4z <paulobrazaraujo@gmail.com>
2024-08-17 13:07:16 -0300

    Update Main component and add images

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,23 @@
 import * as S from './styles'
 
-export function Main() {
+interface IMain {
+  title: string
+  description: string
+}
+
+export function Main({ title, description }: IMain) {
   return (
     <S.Wrapper>
-      <h1>React Avançado</h1>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela com código."
+      />
     </S.Wrapper>
   )
 }
